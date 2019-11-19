165ebcc12f2817ed658279f39c3f715d06d14bb6
Yanying Wang <yanyingwang1@gmail.com>
2019-11-19 16:42:03 +0800

    fix scribble name

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "timable")
 (define deps '("base" "srfi" "gregor" "db"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/time-ext.scrbl" ())))
+(define scribblings '(("scribblings/timable.scrbl" ())))
 (define pkg-desc "extend racket's various time libs and make them be able to work together more smoothly.")
 (define version "0.2")
 (define pkg-authors '("Yanying Wang"))
