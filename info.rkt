6c3eabdf5b4365ebc39c0eba4a7141082d3e3d5d
Yanying Wang <yanyingwang@users.noreply.github.com>
2021-02-10 00:04:14 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "timable")
 (define deps '("base" "srfi" "gregor" "db"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/timable.scrbl" ())))
+(define scribblings '(("scribblings/timable.scrbl" (multi-page))))
 (define pkg-desc "extend racket's various time libs.")
 (define version "0.2")
 (define pkg-authors '("Yanying Wang"))
